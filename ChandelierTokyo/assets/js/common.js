f531abf9bcde6adb82c53d3398bb78a265f60e08
TokuhiroKujiraoka <kk.t.kk11@gmail.com>
2020-09-19 18:56:37 +0900

    デザイン修正

--- ChandelierTokyo/assets/js/common.js
+++ ChandelierTokyo/assets/js/common.js
@@ -57,10 +57,13 @@ $(function () {
 
 $('.slick01').slick({
     autoplay: true, //自動再生
-    dots: true, //ドットのナビゲーションを表示
+    dots: false, //ドットのナビゲーションを表示
     infinite: true, //スライドのループ有効化
     speed: 2500, //切り替えのスピード（小さくすると速くなる）
     autoplaySpeed: 2500, //自動再生のスピード
     fade: true, //フェードの有効化
+    arrows :false
 });
+
+
 
