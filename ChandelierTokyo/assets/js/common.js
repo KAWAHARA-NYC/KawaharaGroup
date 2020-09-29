075767a0910209d7c4b0026c931b5d7ab4240759
Kanegon7010 <yogaaketazeyo7010@gmail.com>
2020-09-29 18:16:53 +0900

    improve responsive design

--- ChandelierTokyo/assets/js/common.js
+++ ChandelierTokyo/assets/js/common.js
@@ -20,37 +20,7 @@ $(function () {
     });
 });
 
-//ヘッダーロゴの位置変更
-$(function () {
-    function changeheader() {
-        $('.headerch').each(function () {
-            var target = $(this).offset().top;
-            var scroll = $(window).scrollTop();
-            var windowWidth = $(window).width();
-            //ターゲットまでスクロール、画面幅がスマホではない場合のみ動作
-            if ((scroll > target ) && (windowWidth > 768)) {
-                $('.englishname').css('position', 'static');
-                $('.englishname').css('transform', 'translate(0, 0)');
-                $('.englishname').css('-webkit-transform', 'translate(0, 0)');
-                $('.englishname').css('-ms-transform', 'translate(0, 0)');
-                $('.japanesename').css('position', 'static');
-                $('.japanesename').css('transform', 'translate(0, 0)');
-                $('.japanesename').css('-webkit-transform', 'translate(0, 0)');
-                $('.japanesename').css('-ms-transform', 'translate(0, 0)');
-            }else if ((scroll < target) || (windowWidth > 768)) {
-                $('.englishname').css('position', 'absolute');
-                $('.englishname').css('transform', 'translate(-50%, -50%)');
-                $('.englishname').css('-webkit-transform', 'translate(-50%, -50%)');
-                $('.englishname').css('-ms-transform', 'translate(-50%, -50%)');
-                $('.japanesename').css('position', 'absolute');
-                $('.japanesename').css('transform', 'translate(-50%, -50%)');
-                $('.japanesename').css('-webkit-transform', 'translate(-50%, -50%)');
-                $('.japanesename').css('-ms-transform', 'translate(-50%, -50%)');
-            }
-        });
-    }
-    changeheader();
-    $(window).scroll(function () {
-        changeheader();
-    });
+$('.nav_toggle').on('click', function () {
+    $('.nav_toggle, .global_navi_wrapper').toggleClass('show');
+    $('.global_navi_wrapper').toggleClass('is-hidden');
 });
